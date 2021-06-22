1779bdadc7c7fae2cddaa1d94c47fdce3a9e728a
Harshada Ahirrao <70407162+HarShu922@users.noreply.github.com>
2021-06-22 12:55:04 +0530

    Update CountCombinations.java

--- src/main/java/exam/CountCombinations.java
+++ src/main/java/exam/CountCombinations.java
@@ -4,6 +4,8 @@ public class CountCombinations {
 
   public int getNumberOfWays(final int total, final int k) {
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
+    
+
     return 0;
   }
 
